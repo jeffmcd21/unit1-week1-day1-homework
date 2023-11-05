9c1cb8a58787a89d686236fba5fd4616425fdae7
Jeff McDonald <jwmcdonald85@gmail.com>
2023-11-05 07:09:50 -0500

    First Homework, First Commit

---
+++ first_js_homework.js
@@ -0,0 +1,5 @@
+// Section 1
+    // 1. DRY means "Don't Repeat Yourself" i.e. avoid repeating code whenver possible. We should pay attention to it for the benefit of maintenance, readability, 
+    //      less code required, better performance and execution. The use of Functions, Modules, Variables, & Abstract are all useful tools for writing DRY code.
+
+    // 2. const: Block scope, Immutable(cannot be reassigned); let: Block scope, mutable (can be reassigned); var: Function or global scope, mutable (can be reassigned)
