54055cd4c4ab6199069f35e30480d6dbcce30e13
Jeff McDonald <jwmcdonald85@gmail.com>
2023-11-06 20:16:28 -0500

    first homework answered

--- first_js_homework.js
+++ first_js_homework.js
@@ -3,3 +3,143 @@
     //      less code required, better performance and execution. The use of Functions, Modules, Variables, & Abstract are all useful tools for writing DRY code.
 
     // 2. const: Block scope, Immutable(cannot be reassigned); let: Block scope, mutable (can be reassigned); var: Function or global scope, mutable (can be reassigned)
+
+// Section 2
+    // Boolean expressions
+
+    {
+        const a = 4;
+        const b = 53;
+        const c = 57;
+        const d = 16;
+        const e = 'Kelvin';
+        const f = false;
+
+        console.log(a < b);
+        console.log(c > d);
+        console.log('Name' === 'Name');
+        console.log(a + b === c);
+        console.log(a * a === d);
+        console.log(e !== 'Kevin');
+        console.log(48 !== '48');
+        
+        let g = 0;  // I use let because const value cannot be changed.
+        console.log(g);
+        g = (b + c);
+        console.log(g);
+    
+
+    
+        g = 0 
+        console.log(g); // After testing this without (const, let or var) I did not get an error.
+    
+
+    // {
+    //     10 = g
+    //     console.log(g); // After testing this I did get an error, which is why its commented out.
+    // }
+
+
+// -----------------------------------------------------------Come back to this-----------------------------------------------------------//
+    // Bonus
+    // a _ (b || f) _ !f && e _ c
+    console.log(a > (b || f) < !f && e !== c);
+// -----------------------------------------------------------Come back to this-----------------------------------------------------------//
+}
+
+// Section 3
+    // While loops
+    // Infinite loop
+        // Answer: This code appears to be an infinite loop because true will always be true, which means this code will never hit a "false" stopping point.
+    
+    // Infinite loop II
+        // Answer: Yes this is an infinite loop because the update to runProgram will fail because the variable was setup with "const" not "let" or "var".
+
+    // Reading code
+    
+    // assigns the variable letters the value A
+    let letters = "A";
+    // assigns the variable i the value 0
+    let i = 0;
+
+    // starts a while loop that will run as long as i is less than 20
+    while (i < 20) {
+        // assigns letters another increment of A now equal to AA.
+	    letters += "A";
+        // increments the value of i by 1
+	    i++;
+    // closes the while loop, the code inside this loop will keep running until the while condition is exaluaded to false
+    }
+    // prints the value of the variable letters to the console.
+    console.log(letters);
+
+    // I didn't expect letter to increment as long as i continued to increment, my expectation was letters would only run once. 
+    // After looking back and realizing [letters += "A"] was inside the {} it makes sense that it would continue to run as long as i was less than 20.
+
+// Section 4
+    // For loops
+        // Answer: 
+            // Differences: For loops are generally used if you know the number of expected iterations and While loops are generally used if you do not know the number of expected iterations.
+                        //  For loops are more rigid in structure and While loops are more flexible in structure.
+            // Similarities: Both For and While loops are useful to run iterative tasks and run if a condition is met and stop when the condition becomes false.
+
+    // For loop control panel
+        // Answer: 
+            // The first part of the control panel is: initialization; 
+            // The first part of the control panel is: condition;
+            // The first part of the control panel is: increment;
+               // Note: I wanted to call the initialization "variable or assignment" but that didn't seem correct so I looked up the answer to confirm. 
+    
+    // For loop II
+
+    for (let i = 0; i < 1000; i++) {
+        console.log(i);
+    }
+
+// -----------------------------------------------------------Come back to this-----------------------------------------------------------//
+    // Bonus
+
+    // The backslash is used to allow a single quote inside a sting that starts and ends with a single quote without error.
+
+// -----------------------------------------------------------Come back to this-----------------------------------------------------------//
+
+
+    // For loop in reverse
+
+    for (let i = 999; i >= 0; i--) {
+        console.log(i);
+    }
+
+    // More counting
+
+    for (let i = 1; i < 11; i++) {
+        console.log("The value of i is: " + i + " of 10");
+    }
+
+
+// -----------------------------------------------------------Bonus-----------------------------------------------------------//
+    // Bonus
+
+    for (let i = 1; i < 11; i++) {
+        console.log(`The value of i is: ${i} of 10`);
+    }
+// -----------------------------------------------------------Bonus-----------------------------------------------------------//
+
+
+    // Iteration
+
+    const StarWars = ["Han", "C3PO", "R2D2", "Luke", "Leia", "Anakin"];
+
+    for (let i = 0; i < StarWars.length; i++) {
+        console.log(StarWars[i]);
+    }
+
+
+// -----------------------------------------------------------Bonus-----------------------------------------------------------//    
+    // Bonus:
+
+    for (let i = 0; i < StarWars.length; i = i + 2) {
+        console.log(StarWars[i]);
+    }
+
+// -----------------------------------------------------------Bonus-----------------------------------------------------------//
--- terminal_homework.bash
+++ terminal_homework.bash
@@ -3,57 +3,66 @@
 # Open the **Terminal app**
 
 # Create a new directory on your desktop called `galaxy_far_far_away`and enter it
-
+    jeffmcdonald@Jeffs-MacBook-Pro Desktop % mkdir galaxy_far_far_away
+    jeffmcdonald@Jeffs-MacBook-Pro Desktop % cd galaxy_far_far_away
 # Create a directory called `death_star`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mkdir death_star
 # and make the following files inside of it:
 # darth_vader.txt
 # princess_leia.txt
 # storm_trooper.txt
-
+    jeffmcdonald@Jeffs-MacBook-Pro death_star % touch darth_vadar.txt princess_leia.txt storm_trooper.txt
 # In `galaxy_far_far_away`, make a directory named `tatooine`
-
+    jeffmcdonald@Jeffs-MacBook-Pro death_star % cd ..
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mkdir tatooine
 # and create the following files in it:
 # luke.txt
 # ben_kenobi.txt
-
-# Inside of `tatooine` make a directory called `millenium_falcon`
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % touch ./tatooine/luke.txt ./tatooine/ben_kenobi.txt
+# Inside of `tatooine` make a directory called `millenium_falcon` 
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mkdir ./tatooine/millenium_falcon
 # and in it create:
 # han_solo.txt
 # chewbaca.txt
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % touch ./tatooine/millenium_falcon/han_solo.txt ./tatooine/millenium_falcon/chewbaca.txt
 # Rename `ben_kenobi.txt` to `obi_wan.txt
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mv ./tatooine/ben_kenobi.txt ./tatooine/obi_wan.txt
 # Copy `storm_trooper.txt` from `death_star` to `tatooine`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % cp ./death_star/storm_trooper.txt ./tatooine/storm_trooper.txt
 # Move `luke.txt` and `obi_wan.txt` to the `millenium_falcon`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mv ./tatooine/luke.txt ./tatooine/obi_wan.txt ./tatooine/millenium_falcon
 # Move `millenium_falcon` out of `tatooine` and into `galaxy_far_far_away`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mv ./tatooine/millenium_falcon ../galaxy_far_far_away
+    # got hung up on this one.
 # Move `millenium_falcon` into `death_star`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mv millenium_falcon ./death_star
 # Move `princess_leia.txt` into the `millenium_falcon`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mv ./death_star/princess_leia.txt ./death_star/millenium_falcon
 # Delete `obi_wan.txt`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % rm ./death_star/millenium_falcon/obi_wan.txt
 # In `galaxy_far_far_away`, make a directory called `yavin_4`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mkdir yavin_4
 # Move the `millenium_falcon` out of the `death_star` and into `yavin_4`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mv ./death_star/millenium_falcon ./yavin_4
 # Make a directory in `yavin_4` called `x_wing`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mkdir ./yavin_4/x_wing
 # Move `princess_leia.txt` to `yavin_4` and `luke.txt` to `x_wing`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mv ./yavin_4/millenium_falcon/princess_leia.txt ./yavin_4 
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mv ./yavin_4/millenium_falcon/luke.txt ./yavin_4/x_wing
+    # attempted to move both files at the sametime and didn't yield the correct results.
+        # // jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mv ./yavin_4/millenium_falcon/princess_leia.txt ./yavin_4 ./yavin_4/millenium_falcon/luke.txt ./yavin_4/x_wing
 # Move the `millenium_falcon` and `x_wing` out of `yavin_4` and into `galaxy_far_far_away`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mv ./yavin_4/millenium_falcon ../galaxy_far_far_away ./yavin_4/x_wing ../galaxy_far_far_away
 # In `death_star`, create directories for `tie_fighter_1`, `tie_fighter_2` and `tie_fighter_3`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mkdir ./death_star/tie_fighter_1 ./death_star/tie_fighter_2 ./death_star/tie_fighter_3
 # Move `darth_vader.txt` into `tie_fighter_1`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mv ./death_star/darth_vadar.txt ./death_star/tie_fighter_1
 # Make a copy of `storm_trooper.txt` in both `tie_fighter_2` and `tie_fighter_3`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % cp ./death_star/storm_trooper.txt ./death_star/tie_fighter_2
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % cp ./death_star/storm_trooper.txt ./death_star/tie_fighter_3
+    # unsuccessful at coping both at the sametime
 # Move all of the `tie_fighters` out of the `death_star` and into `galaxy_far_far_away`
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mv ./death_star/tie_fighter_1 ../galaxy_far_far_away ./death_star/tie_fighter_2 ../galaxy_far_far_away ./death_star/tie_fighter_3 ../galaxy_far_far_away
 # Be careful with this command - cannot undo!
 
 # Make sure you delete the right thing, or you could accidentally delete the contents of your computer (it has happened).
@@ -61,11 +70,26 @@
 # This command will typically not ask you if you really want to delete. It will just delete so doublecheck you are deleting exactly what you mean to delete
 
 # Remove `tie_fighters` 2 and 3.
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % rm -r -i ./tie_fighter_2
+    examine files in directory ./tie_fighter_2? y
+    remove ./tie_fighter_2/storm_trooper.txt? y
+    remove ./tie_fighter_2? y
 
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % rm -r -i ./tie_fighter_3
+    examine files in directory ./tie_fighter_3? y
+    remove ./tie_fighter_3/storm_trooper.txt? y
+    remove ./tie_fighter_3? y
 # Touch a file in "**x_wing**" called "**the_force.txt**".
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % touch ./x_wing/the_force.txt
 # Destroy the "**death_star**" and anyone inside of it.
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % rm -r -i ./death_star 
+    examine files in directory ./death_star? y
+    # remove ./death_star/.DS_Store? y
+    remove ./death_star/storm_trooper.txt? y
+    remove ./death_star? y
 # Return "**x_wing**" and the "**millenium_falcon**" to "**yavin_4**".
-
+    jeffmcdonald@Jeffs-MacBook-Pro galaxy_far_far_away % mv ./x_wing ./yavin_4 ./millenium_falcon ./yavin_4 
 # Celebrate!
+
+
+
